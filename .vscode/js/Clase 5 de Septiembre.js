05fe972a82e72d63c88cd6e44166cb5a4e1c3150
BrunoGiugno26 <brunogiugno@gmail.com>
2023-09-07 15:08:21 -0300

    Clase 5 de septiembre con los ejercisios terminados

--- .vscode/js/Clase 5 de Septiembre.js
+++ .vscode/js/Clase 5 de Septiembre.js
@@ -50,18 +50,11 @@ for(const indice in herramientas) {
 /* CUADRO DE FOR IN  -  FOR OF Y SUS DEVOLUCIONES
 
                     QUE PUEDO RECORRER           QUE ME DEVUELVE EN CADA INTERACION
+
 *FOR IN:             ARRAYS|OBJETOS                  INDICE|PROPIEDAD
 *FOR OF:                  ARRAYS                     CONTENIDO DEL INDICE
 
 */
-
-//? EJERCICIOS: RECONOCER QUE TIPO DE ESTRUCTURA ES Y QUE PODRIAMOS UTILIZAR:
-
-const mascotas = ["Firulais","Momo","Pochi"] //ARRAY
-const deptos = [{piso:3,depto:"A"},{piso:1,depto:"C"}] //ARRAY CON OBJETO
-const empleados = {nombre:"Jose Luis",area: "Tesoreria",antiguedad: "5 años"} //OBJETO (puede contener ARRAYS)
-const calle = {nombre:"Benito Juarez", altura:1556} // OBJETO (puede contener ARRAYS)
-
 
 /*
                         FOR-IN              FOR-OF                 DEVOLUCIONES EN CADA RECORRIDO
@@ -73,3 +66,42 @@ calle                    SI                    NO            => for in: una PROP
 */
 
 //* HOMEWORK: a cada una de las estructuras creadas, recorrerlas con un FOR-IN o FOR-OF segun correspondan. Dentro de cada recorrido, simplemente poner un console.log para ver la devolucion de cada estructura. EN EL FOR IN TRATAR DE DEVOLVER AMBAS COSAS EN MI ESTRUCTURA RECORRIDA (EJ: en mi array devolver no solo el INDICE, sino el CONTENIDO)
+
+//? EJERCICIOS: RECONOCER QUE TIPO DE ESTRUCTURA ES Y QUE PODRIAMOS UTILIZAR:
+
+//FOR IN: Va a dar una devolucion del indice ya que es un ARRAY,en este caso las mascotas. Tambien del contenido que hay dentro del INDICE.
+const mascotas = ["Firulais","Momo","Pochi"]
+for (const indice in mascotas) {
+    console.log(mascotas[indice]);
+}
+
+//FOR OF: RECORRE EL CONTENIDO DEL ELEMENTO
+//const mascotas = ["Firulais","Momo","Pochi"] //ARRAY 
+//for(const elemento of mascotas)
+//console.log(elemento)
+
+
+//FOR IN: Va a mostrar solamente el contenido ya que en este ejemplo de los deptos es un array con objetos lo que dice de que en lugar de indices tiene PROPIEDADES. 
+const deptos = [{piso:3,depto:"A"},{piso:1,depto:"C"}] //ARRAY CON OBJETO
+for (const propiedad in deptos) {
+    console.log(deptos[propiedad])
+}
+
+//FOR OF: va a mostarar los elementos de cada objeto que haya dentro
+
+//const deptos = [{piso:3,depto:"A"},{piso:1,depto:"C"}]
+//for(const elemento of deptos)
+//console.log(elemento)
+
+
+//FOR IN: en este ejemplo de los empleados solamente se realiza un for in ya que no posee INDICE
+const empleados = {nombre:"Jose Luis",area: "Tesoreria",antiguedad: "5 años"} //OBJETO (puede contener ARRAYS)
+for(llave in empleados){
+    console.log(`propiedad:${llave},contenido: ${empleados [llave]}`)
+}
+
+//FOR IN: ocurre lo mismo que en el ejemplo anterior esta vez con la calle.
+const calle = {nombre:" Benito Juarez", altura:" 1556"} // OBJETO (puede contener ARRAYS)
+for(llave in calle){
+    console.log(`propiedad:${llave}, contenido${calle [llave]}`)
+}
